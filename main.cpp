e5e86b60cc943d6429ef968062d5170b85af122b
SpecIDontChetPrimeSousCaprisun <corieux1@gmail.com>
2026-07-17 17:49:05 +0200

    Added collisions

--- main.cpp
+++ main.cpp
@@ -8,7 +8,16 @@ int main() {
                    0.0f,
                    glm::vec3(1.0f, 0.0f, 0.0f),
                    0.0f);
-  
+
+  Aleg::Object* platform = new Aleg::Object(glm::vec2(-50.0f, 500.0f), 
+                                            glm::vec2(1000.0f, 100.0f),
+                                            0.0f,
+                                            glm::vec3(1.0f, 0.0f, 0.0f),
+                                            0.0f);
+
+  platform->anchored = true;
+  platform->rotation = 10.0f;
+
   Aleg::mainLoop();
   return 0;
 }
