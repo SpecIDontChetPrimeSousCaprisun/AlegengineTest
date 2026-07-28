d5741f8755182f2a86dd9494e3f0ef4e7b0c800e
SpecIDontChetPrimeSousCaprisun <corieux1@gmail.com>
2026-07-28 13:13:56 +0200

    Finished the scrolling element

--- shaders/Frag.glsl
+++ shaders/Frag.glsl
@@ -7,10 +7,25 @@ uniform float alpha;
 uniform sampler2D tex;
 uniform vec3 color;
 uniform vec3 colorChange;
+uniform vec2 maskPos;
+uniform vec2 maskSize;
+uniform vec2 resolution;
 uniform bool useColor;
+uniform bool hasMask;
 
 void main() {
   vec4 finalColor;
+
+  if (hasMask) {
+    float fragY = resolution.y - gl_FragCoord.y; // flip to top-left origin
+
+    if (!(gl_FragCoord.x > maskPos.x &&
+          gl_FragCoord.x < maskPos.x + maskSize.x &&
+          fragY > maskPos.y &&
+          fragY < maskPos.y + maskSize.y)) {
+      discard;
+    }
+  }
 
   if (useColor) {
     finalColor = vec4(color, alpha);
--- shaders/TextFrag.glsl
+++ shaders/TextFrag.glsl
@@ -6,9 +6,24 @@ varying vec2 TexCoord;
 uniform sampler2D tex;
 uniform vec3 color;
 uniform vec3 colorChange;
+uniform vec2 maskPos;
+uniform vec2 maskSize;
+uniform vec2 resolution;
 uniform float transparency;
+uniform bool hasMask;
 
 void main() {
+  if (hasMask) {
+    float fragY = resolution.y - gl_FragCoord.y; // flip to top-left origin
+
+    if (!(gl_FragCoord.x > maskPos.x &&
+          gl_FragCoord.x < maskPos.x + maskSize.x &&
+          fragY > maskPos.y &&
+          fragY < maskPos.y + maskSize.y)) {
+      discard;
+    }
+  }
+
   float textAlpha = texture2D(tex, TexCoord).a;
   gl_FragColor = vec4(color + colorChange, textAlpha - transparency);
 }
